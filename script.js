1919fb2a97f76c9d237f5fae7de9b1e137668e0b
Your Elina <Your elina.marcol@gmail.com>
2024-11-06 15:15:41 +0100

    frontend css html

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector("#btn-search").addEventListener("click", function () {
+    let searchTravel = document.querySelector('#result-trip').innerHTML +=  }
+
+
+document.querySelector("#booking").addEventListener("click", function () {
+})
+
+
+document.querySelector("#cart").addEventListener("click", function () {
+window.location.href = 'panier.html'
+ })
